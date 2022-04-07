527dae1e6e6fd2d3ceb6731fbb38f499f7a40477
Moeketsi Tsotetsi <tmoeketsid@gmail.com>
2022-04-07 14:54:44 +0200

    Finished designing nav bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { PurchaseComponent } from './purchase/purchase.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PurchaseComponent
   ],
   imports: [
     BrowserModule,
